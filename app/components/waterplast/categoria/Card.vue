15e82fa9a8f7657c61115df7c0bb28a255fbc3f7
Emilio Bensignor <liobensignor@gmail.com>
2025-12-18 17:53:46 -0300

    Nombre + Litraje en CategoriaCard

--- app/components/waterplast/categoria/Card.vue
+++ app/components/waterplast/categoria/Card.vue
@@ -2,7 +2,9 @@
     <div class="flex flex-col items-center gap-2">
         <NuxtImg v-if="producto.imagen_principal" :src="producto.imagen_principal" :alt="producto.nombre" class="w-72 md:w-96 lg:w-[20.5rem] xxl:w-[28rem] h-[13.5rem] md:h-[20.75rem] lg:h-[17.5rem] xxl:h-[21.25rem] object-contain" />
         <div class="w-full flex flex-col justify-center items-center gap-4 lg:gap-8 px-5 lg:px-2 pb-4">
-            <HeadingH3 class="text-center text-primary font-bold">{{ producto.nombre }}</HeadingH3>
+            <HeadingH3 class="text-center text-primary font-bold">
+                {{ producto.capacidad_lts ? `${producto.nombre} - ${producto.capacidad_lts}L` : producto.nombre }}
+            </HeadingH3>
             <div v-if="hasCaracteristicas" class="flex flex-wrap justify-center items-end gap-4 lg:gap-6">
                 <div v-if="producto.caracteristica1 && producto.icono1" class="flex flex-col xxl:flex-row items-center gap-2">
                     <div class="w-12 h-12 flex justify-center items-center rounded-full" :style="{ backgroundColor: producto.categoria?.color }">
