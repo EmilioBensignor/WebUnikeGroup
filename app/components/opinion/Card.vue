fac643a6105a6a8079a280436acb0ad7b099f433
Emilio Bensignor <liobensignor@gmail.com>
2025-12-30 11:23:23 -0300

    Fixes Producto y Opinion Card

--- app/components/opinion/Card.vue
+++ app/components/opinion/Card.vue
@@ -1,13 +1,11 @@
 <template>
     <div class="flex flex-col gap-2 bg-white shadow-md shadow-dark/20 rounded-2xl lg:rounded-3xl p-4 xxl:p-6">
         <div class="flex items-center gap-3">
-            <div class="w-12 h-12 rounded-full overflow-hidden">
-                <img :src="avatarUrl" :alt="nombreUsuario" class="w-full h-full object-cover" />
-            </div>
+            <img :src="avatarUrl" :alt="nombreUsuario" class="w-12 h-12 rounded-full object-cover flex-shrink-0" />
             <div class="flex flex-col gap-0.5">
                 <div class="flex items-start gap-0.5 lg:gap-1">
                     <p class="text-xs lg:text-base text-terciary font-semibold">{{ nombreUsuario }}</p>
-                    <Icon name="material-symbols:verified" class="w-3 h-3 text-primary -mt-0.5" />
+                    <Icon name="material-symbols:verified" class="w-3 h-3 flex-shrink-0 text-primary -mt-0.5" />
                 </div>
                 <div class="flex items-center gap-0.5">
                     <Icon v-for="n in Math.floor(ratingValue)" :key="`full-${n}`" name="material-symbols:star-rounded"
